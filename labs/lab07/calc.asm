fd9bc393c22d1e8a12bf0392c645bfa6d97e7d27
Natalia-Sidorova <115586337+Natalia-Sidorova@users.noreply.github.com>
2022-12-13 18:52:30 +0300

    feat(main): add files lab-7

---
+++ labs/lab07/calc.asm
@@ -0,0 +1,31 @@
+%include 'in_out.asm'
+SECTION .data
+msg: DB 'Введите X ',0
+rem: DB 'выражение = : ',0
+SECTION .bss
+x: RESB 80
+SECTION .text
+GLOBAL _start
+_start:
+mov eax, msg
+call sprintLF
+mov ecx, x
+mov edx, 80
+call sread
+mov eax,x ; вызов подпрограммы преобразования
+call atoi ; ASCII кода в число, `eax=x`
+
+mov ebx,8
+mul ebx
+add eax,6
+mov ebx,10
+mul ebx
+
+mov ebx,eax
+mov eax,rem
+call sprint
+mov eax,ebx
+call iprintLF
+call quit
+
+
